3c54d3cdd225b4546bd1a1c5934de16f9b940994
Josue Garcia <bitzgarcia1996@gmail.com>
2024-05-28 20:31:52 +0800

    minor changes

--- client/src/components/logoCreation/logoCreation.component.jsx
+++
@@ -1,7 +0,0 @@
-import "./logoCreation.styles.scss";
-
-const LogoCreation = () => {
-  return <div>Create logo here</div>;
-};
-
-export default LogoCreation;
--- client/src/routes/logo/logo.routes.jsx
+++ client/src/routes/logo/logo.routes.jsx
@@ -1,13 +1,5 @@
-import LogoCreation from "../../components/logoCreation/logoCreation.component";
-import LogoSidebar from "../../components/logoSidebar/logoSidebar.component";
-
 const Logos = () => {
-  return (
-    <>
-      <LogoSidebar />
-      <LogoCreation />
-    </>
-  );
+  return <div>Hey</div>;
 };
 
 export default Logos;
